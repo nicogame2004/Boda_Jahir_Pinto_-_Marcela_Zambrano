26b8115339d44ac329017e3ffb442ac7b4bc97a4
nicogame2004 <nicolas28c05@gmail.com>
2026-03-10 19:35:12 -0500

    Actualización invitación boda

--- js/vestimenta.js
+++ js/vestimenta.js
@@ -2,7 +2,18 @@ window.initVestimenta = function () {
   document.body.style.overflow = "auto";
 
   const btn = document.getElementById("btnVolverInvitacion");
+
   if (btn) {
-    btn.addEventListener("click", () => window.navigate("invitacion"));
+    btn.addEventListener("click", async () => {
+      await window.navigate("invitacion");
+
+      // esperar a que cargue el HTML
+      setTimeout(() => {
+        const el = document.getElementById("dresscode");
+        if (el) {
+          el.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 100);
+    });
   }
 };
